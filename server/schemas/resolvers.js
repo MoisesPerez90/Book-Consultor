5ea615320884e6a2cefd8a77f0a9315827640400
Moises Saul Perez Rodriguez <mperez@americas.phoenixcontact.com>
2023-05-15 21:31:58 -0600

    Setting the schemas folder, where I'm creating the resolvers and typeDefs files

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,79 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().populate("savedBooks");
+    },
+
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate("savedBooks");
+    },
+    books: async (parent, { username }) => {
+      const user = User.findOne({ username }).populate("savedBooks");
+      return user.savedBooks;
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        data = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return data;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { user, token };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { user, token };
+    },
+    saveBook: async (parent, { book }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: book } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
+        return updatedUser;
+      } catch (err) {
+        throw new Error("Unable to remote the book. Please try again.");
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
